ec8a847d465f6e355aa31cfac05f051ad3fefce4
Aurora Avery <aurora.avery@yahoo.com>
2020-04-19 07:08:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
 def unsafe?(speed)
-
+  speed < 40 || speed > 60 ? true : false 
 end
-
-
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false 
 end
+
+def sum_machine(first, second)
+  if first < second
+    low = first
+    
+  sum
+end
+
+def squares(collection)
+  collection.collect do |num|
+     num ||= 0
+
 	
 
 
